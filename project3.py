229bdea9d10852589d4556e3bc2fccdb442fb816
George Lindner <dglindner2@gmail.com>
2020-10-25 20:14:19 -0400

    first attempt

--- project3.py
+++ project3.py
@@ -19,8 +19,49 @@ detectArbitrage
 """
 def detectArbitrage(adjList, adjMat, tol=1e-15):
     ##### Your implementation goes here. #####
-    return []
-    ##### Your implementation goes here. #####
+    # Set initial dist and prev.
+
+    cycle = None
+
+    for vertex in adjList:
+        vertex.dist = math.inf
+        vertex.prev = None
+
+    # NOTE THIS COULD BE AN ISSUE
+    # Initialize start distance
+    adjList[0].dist = 0
+
+    # iterate |V| - 1 times
+    for i in range(len(adjList) - 1):
+        for vertex in adjList:
+            # Check each neighbor of the vertex
+            for neighbor in vertex.neigh:
+                # only update if new value is better
+                if neighbor.dist > vertex.dist + adjMat[vertex.rank][neighbor.rank] + tol:
+                    neighbor.dist = vertex.dist + adjMat[vertex.rank][neighbor.rank]
+                    neighbor.prev = vertex
+
+# NOTE: OUR CURRENT ERROR IS BECAUSE THIS IF STATEMENT IN LINE 46 IS NEVER TRUE
+    for vertex in adjList:
+        for neighbor in vertex.neigh:
+            if neighbor.dist > vertex.dist + adjMat[vertex.rank][neighbor.rank] + tol:
+                # This means that there is a cycle
+                cycle = neighbor
+                break
+
+    path = []
+
+    if cycle is None:
+        return []
+    else:
+        while cycle.prev is not None:
+            path.append(cycle.prev)
+            cycle = cycle.prev
+
+    # Reverse list
+    path = path[::-1]
+
+    return path
 
 ################################################################################
 
@@ -28,7 +69,7 @@ def detectArbitrage(adjList, adjMat, tol=1e-15):
 rates2mat
 """
 def rates2mat(rates):
-    ##### Your implementation goes here. #####
+    # WHAT DOES THIS FUNCTION NEED TO DO
     # Currently this only returns a copy of the rates matrix.
     return [[R for R in row] for row in rates]
     ##### Your implementation goes here. #####
