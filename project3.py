e475e9c6495ddcbf3500ce380285ea0f061ab4e0
ezinne359 <enwankwo17@gmail.com>
2020-10-28 21:11:59 -0400

    final code, added comments

--- project3.py
+++ project3.py
@@ -3,9 +3,9 @@ Math 560
 Project 3
 Fall 2020
 
-Partner 1:
-Partner 2:
-Date:
+Partner 1: George Lindner (dgl12)
+Partner 2: Ezinne Nwankwo (esn11)
+Date: October 28, 2020
 """
 
 # Import math and p3tests.
@@ -17,35 +17,38 @@ from p3tests import *
 """
 detectArbitrage
 """
+
+
 def detectArbitrage(adjList, adjMat, tol=1e-15):
-    ##### Your implementation goes here. #####
-    # Set initial dist and prev.
+    # Set initial distances and previous nodes.
 
     for vertex in adjList:
         vertex.dist = math.inf
         vertex.prev = None
 
-    # Initialize start distance
+    # Initialize start distance to 0
+    # Choose starting node as first node in adjacency list
     adjList[0].dist = 0
 
     # iterate |V| - 1 times
     for i in range(len(adjList) - 1):
+        # iterate over each  vertex in adjacency list
         for vertex in adjList:
             # Check each neighbor of the vertex
             for neighbor in vertex.neigh:
-                # only update if new value is better
+                # only update if new node distance gives better offer
+                # meaning the distance is shorter
                 if neighbor.dist > vertex.dist + adjMat[vertex.rank][neighbor.rank] + tol:
                     neighbor.dist = vertex.dist + adjMat[vertex.rank][neighbor.rank]
                     neighbor.prev = vertex
 
-
-    # This is our extra iteration.
+    # Run extra iteration to check if any distances changed
+    # If the optimal value is changed, this implies that there is a cycle
+    # Thus, we note which node is changed. And we start from that node
+    # That node is called 'cycle'
     cycle = None
     for vertex in adjList:
         for neighbor in vertex.neigh:
-            # If the optimal value is changed, this implies that there is a cycle.
-            # Thus, we note which node is changed. And we start from that node.
-            # That node is called 'cycle' in our code
             # This if statement only will be true if a distance is updated!
             if neighbor.dist > vertex.dist + adjMat[vertex.rank][neighbor.rank] + tol:
                 # This means that there is a cycle
@@ -53,20 +56,24 @@ def detectArbitrage(adjList, adjMat, tol=1e-15):
                 neighbor.prev = vertex
                 cycle = neighbor
                 break
-
+        # As soon as the cycle is not empty, meaning a node was updated, indicating arbitrage
         if cycle is not None:
             break
 
-
+    # If cycle is none, then no arbitrage and return empty list
     if cycle is None:
         return []
-
+    # If cycle is not none, then we need to traceback the cycle of nodes that had arbitrage
     path = []
     # While the current vertex is not already in the path
+    # append the previous nodes to path
+    # this returns list of the nodes that were updated after extra iteration
     while cycle.rank not in path:
         path.append(cycle.rank)
         cycle = cycle.prev
 
+    # Now we need to work back to get actual arbitrage cycle
+    # and remove any extra nodes not in arbitrage cycle
     cycle = cycle.prev
     path = []
     while cycle.rank not in path:
@@ -77,15 +84,19 @@ def detectArbitrage(adjList, adjMat, tol=1e-15):
 
     return path[::-1]
 
+
 ################################################################################
 
 """
 rates2mat
 """
+
+
 def rates2mat(rates):
     # Currently this only returns a copy of the rates matrix.
     return [[-math.log(R) for R in row] for row in rates]
     ##### Your implementation goes here. #####
+
 
 """
 Main function.
